6b830e708c7cbf1fdc62791003b1994416dea3af
19dgstec <19dg.jakub.stec@ezse.pl>
2021-03-02 13:11:39 +0100

    widok glosowania

--- polls/views.py
+++ polls/views.py
@@ -2,4 +2,15 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Patrzysz na index glosowania.")
+
+def detail(request, question_id):
+	return HttpResponse("Patrzysz na pytanie %s." % question_id)
+
+def results(request, question_id):
+	response = "Patrzysz na pytanie %s."
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("Glosujesz na pytanie %s." % question_id)
