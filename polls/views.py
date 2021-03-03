53715240f210c2be9d3107c64446634c630f247d
19dgstec <19dg.jakub.stec@ezse.pl>
2021-03-03 09:39:49 +0100

    views, templates, errors

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("Patrzysz na index glosowania.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
-	return HttpResponse("Patrzysz na pytanie %s." % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html',{'question':question})
+
 
 def results(request, question_id):
 	response = "Patrzysz na pytanie %s."
@@ -14,3 +24,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("Glosujesz na pytanie %s." % question_id)
+
