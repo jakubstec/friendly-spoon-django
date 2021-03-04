8473f7677bf7653fd3e88bf4ecaea1add6015196
19dgstec <19dg.jakub.stec@ezse.pl>
2021-03-04 03:11:28 +0100

    generic views

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,28 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Question
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+from .models import Choice, Question
 
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html',{'question':question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def results(request, question_id):
-	response = "Patrzysz na pytanie %s."
-	return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
-	return HttpResponse("Glosujesz na pytanie %s." % question_id)
-
+    return HttpResponse("You are voting on question: %s." % question_id)
